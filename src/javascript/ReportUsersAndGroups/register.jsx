78acf396c6d0e5056f2503d534f57ba3425d5873
Florent BOURASSE <fbourasse@jahia.com>
2026-04-25 21:58:32 +0200

    Change of parent item in the Jahia admin
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- src/javascript/ReportUsersAndGroups/register.jsx
+++ src/javascript/ReportUsersAndGroups/register.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default () => {
     console.debug('%c report-users-and-groups: activation in progress', 'color: #005580');
     registry.add('adminRoute', 'reportUsersAndGroups', {
-        targets: ['administration-server-systemComponents:10'],
+        targets: ['administration-server-usersAndRoles:99'],
         requiredPermission: 'admin',
         label: 'report-users-and-groups:label.menu_entry',
         isSelectable: true,
