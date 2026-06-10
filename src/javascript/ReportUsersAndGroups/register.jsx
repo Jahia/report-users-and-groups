2ae9194c811f797f73abd9b806388079348b7a1d
Florent BOURASSE <fbourasse@jahia.com>
2026-06-10 11:38:48 +0200

    fix: update requiredPermission to module-specific permission in frontend registration
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- src/javascript/ReportUsersAndGroups/register.jsx
+++ src/javascript/ReportUsersAndGroups/register.jsx
@@ -6,7 +6,7 @@ export default () => {
     console.debug('%c report-users-and-groups: activation in progress', 'color: #005580');
     registry.add('adminRoute', 'reportUsersAndGroups', {
         targets: ['administration-server-usersAndRoles:99'],
-        requiredPermission: 'admin',
+        requiredPermission: 'reportUsersAndGroupsAdmin',
         label: 'report-users-and-groups:label.menu_entry',
         isSelectable: true,
         render: () => React.createElement(ReportUsersAndGroupsAdmin)
